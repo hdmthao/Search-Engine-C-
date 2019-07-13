43d6b3e4bec921061384b9149f43d045cad09d8c
lahoangphong <lahoangphong@gmail.com>
2019-07-13 08:35:50 +0700

    AutoCorrect Feature

--- src/speller/speller.cc
+++ src/speller/speller.cc
@@ -1,1 +1,115 @@
 #include "speller.h"
+#include "../util/util.h"
+#include <iostream>
+
+Speller::Speller(Searcher* searcher) : searcher(searcher), dict(nullptr) {
+    dict = new Trie();
+}
+
+Speller::~Speller() {
+    SAFE_DELETE(searcher);
+    SAFE_DELETE(dict);
+}
+
+bool Speller::Check(const std::string & origin, std::string & fix){
+	fix = origin;
+	if (Search(fix))
+		return true;
+	Insert(fix);
+	std::queue < std::pair<std::string, int> > inCorrectList;
+	inCorrectList.push(make_pair(fix, 0));
+	while (!inCorrectList.empty()) {
+		std::string curWord = inCorrectList.front().first;
+		int rep = inCorrectList.front().second;
+		inCorrectList.pop();
+		// delete a character
+		for (int i = 0; i <= curWord.length(); i++) {
+			std::string newWord = curWord;
+			newWord.erase(i, 1);
+			//std::cout << newWord << "\n";
+			if (Search(newWord)) {
+				fix = newWord;
+				return true;
+			}
+			if (rep < 1 && !dict->IsExistWord(newWord)) {
+				Insert(newWord);
+				inCorrectList.push(make_pair(newWord, rep + 1));
+			}
+		}
+		// change a character
+		for (int i = 0; i < curWord.length(); i++) {
+			for (int j = 'a'; j <= 'z'; j++) {
+				std::string newWord = curWord;
+				newWord[i] = j;
+				//std::cout << newWord << "\n";
+				if (Search(newWord)) {
+					fix = newWord;
+					return true;
+				}
+				if (rep < 0 && !dict->IsExistWord(newWord)) {
+					Insert(newWord);
+					inCorrectList.push(make_pair(newWord, rep + 1));
+				}
+			}
+		}
+
+		// swap 2 characters
+
+		for (int i = 0; i < curWord.length(); i++) {
+			for (int j = i + 1; j < curWord.length(); j++) {
+				std::string newWord = curWord;
+				std::swap(newWord[i], newWord[j]);
+				//std::cout << newWord << "\n";
+				if (Search(newWord)) {
+					fix = newWord;
+					return true;
+				}
+				if (rep < 0 && !dict->IsExistWord(newWord)) {
+					Insert(newWord);
+					inCorrectList.push(make_pair(newWord, rep + 1));
+				}
+			}
+		}
+
+		// add a character
+		std::string newWord = "_" + curWord;
+		for (int j = 'a'; j <= 'z'; j++) {
+			newWord[0] = j;
+			if (Search(newWord)) {
+				fix = newWord;
+				return true;
+			}
+		}
+		for (int i = 1; i <= curWord.length(); i++) {
+			std::swap(newWord[i], newWord[i - 1]);
+			for (int j = 'a'; j <= 'z'; j++) {
+				newWord[i] = j;
+				//std::cout << newWord << "\n";
+				if (Search(newWord)) {
+					fix = newWord;
+					return true;
+				}
+				if (rep < 1 && !dict->IsExistWord(newWord)) {
+					Insert(newWord);
+					inCorrectList.push(make_pair(newWord, rep + 1));
+				}
+			}
+		}
+
+
+	}
+	return false;
+}
+
+
+bool Speller::Insert(std::string &s) {
+    if (dict->IsExistWord(s)) {
+        return false; // this world already exist
+    }
+
+    return dict->Insert(s);
+}
+
+bool Speller::Search(std::string &s) {
+    return searcher->IsExistWord(s);
+}
--- src/speller/speller.h
+++ src/speller/speller.h
@@ -2,16 +2,22 @@
 #define SPELLER_H
 
 #include "../trie/trie.h"
+#include "../searcher/searcher.h"
+#include <string.h>
 
 
 class Speller {
 private:
-
+    Trie* dict; 
+    Searcher* searcher; 
+    bool Insert(std::string &s); // insert s to dict if it not exist in dict
+    bool Search(std::string &s); // return true if there is exist a word in database
 
 public:
-    Speller();
+    Speller(Searcher* searcher);
     ~Speller();
-
+	bool Check(const std::string &origin, std::string &fix);
 };
+
 
 #endif
