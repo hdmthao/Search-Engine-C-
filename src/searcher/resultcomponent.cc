9f83d4ab0ad28972c3848c2c4acfae5366c96c45
nbhlong <nguyenbaohailong@gmail.com>
2019-07-25 18:10:11 +0700

    searcher

---
+++ src/searcher/resultcomponent.cc
@@ -0,0 +1,100 @@
+#include "resultcomponent.h"
+
+ResultComponent *getFile(const std::string &s, std::string filename)
+{
+	std::ifstream fin(filename);
+
+	if (!fin.is_open())
+		return NULL;
+
+	ResultComponent *fil = new ResultComponent;
+	std::string st = s;
+	st = util::string::RemoveUnicode(st);
+	st = util::string::Trim(s);
+	st = util::string::RemoveMark(st);
+
+	std::vector <std::string> words = util::string::Split(st);
+	
+	int max_word = 0;
+	std::string line_max="";
+	std::string line_se="";
+	while (!fin.eof())
+	{
+		std::string str;
+
+		int count = 0;
+		getline(fin, str);
+		std::vector <std::string> word_str = util::string::Split(util::string::Trim(str));
+		
+		
+		if (fil->title == "") {
+			if (str.length() < 100)
+				fil->title = str;			
+			else {
+				if (str.find(".") != -1 && str.find(".") < 120 )
+					fil->title = str.substr(0, str.find("."));
+				else {
+					fil->title = str.substr(0, 100);
+					std::string ti = fil->title;
+					while (ti[ti.length()-1] != ' ')
+						ti.pop_back();
+					ti.pop_back();
+					fil->title = ti + "...";
+				}
+			}
+		}
+		
+
+		for (auto it = words.begin(); it != words.end(); it++) {
+			if (str.find(*it) != -1) {
+				for (auto wo : word_str)
+					if (wo.find(*it)!=-1) {
+						fil->total_keywords++;
+						++count;
+					}
+			}
+		}
+		if (count > max_word) {
+			line_se = line_max;
+			line_max = str;
+			max_word = count;
+		}
+
+	}
+	fin.close();
+	if (line_max.length() < 80)
+		fil->paragraph = line_max + "..." + line_se;
+	else
+		if (line_max.length() < 150)
+			fil->paragraph = line_max;
+		else {
+			if (line_max.find(".") != -1 && line_max.find(".") < 150)
+				fil->paragraph = line_max.substr(0, line_max.find("."));
+			else {
+				fil->paragraph = line_max.substr(0, 150);
+				std::string pa = fil->paragraph;
+				while (pa[pa.length() - 1] != ' ')
+					pa.pop_back();
+				pa.pop_back();
+				fil->paragraph = pa + "...";
+			}
+		}
+
+	std::vector <std::string> word_para = util::string::Split(util::string::Trim(fil->paragraph));
+	std::string para = "";
+	
+	for (auto wo : word_para) {
+		bool ok = false;
+		for (auto it : words)
+			if (wo.find(it) != -1)
+				ok = true;
+		if (ok)
+			para += "<hl>" + wo + "<hl> ";
+		else
+			para += wo + " ";
+	}
+	para.pop_back();
+	fil->paragraph = para;
+	return fil;
+}
+
--- src/searcher/resultcomponent.h
+++ src/searcher/resultcomponent.h
@@ -1,12 +1,20 @@
 #ifndef RESULTCOMPONENT_H
 #define RESULTCOMPONENT_H
 
+#include "../util/util.h"
 
 struct ResultComponent {
+	std::string title;
+	int total_keywords;
+	std::string paragraph;
+	ResultComponent() {
+		title = "";
+		total_keywords = 0;
+		paragraph = "";
+	}
 
-
-    ResultComponent() {}
-    ~ResultComponent() {}
 };
+
+ResultComponent *getFile(const std::string &s, std::string filename);
 
 #endif
